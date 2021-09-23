09dec6827b3a4fcae0fbb0f879cb890506bc675d
mkpoli <snowwolf1001@gmail.com>
2021-09-23 19:09:04 +0900

    Redesign Zoom Indicator

---
+++ src/lib/ZoomIndicator.svelte
@@ -0,0 +1,83 @@
+<script lang="ts">
+  import IconButton from "./basic/IconButton.svelte";
+
+  export let zoom: number
+  export let min: number
+  export let max: number
+  export let step: number
+
+  const ICON_HEIGHT = '2em'
+</script>
+
+<div class="zoom-indicator" title="Zoom">
+  <IconButton
+    attrs={{ icon: 'system-uicons:zoom-in', height: ICON_HEIGHT }}
+    on:click={() => { if (zoom <= max + step)  zoom += step }}
+  />
+  <div class="zoom-range-container">
+    <input type="range" bind:value={zoom} min={min} max={max} step={step}>
+  </div>
+  <IconButton
+    attrs={{ icon: 'system-uicons:zoom-out', height: ICON_HEIGHT }}
+    on:click={() => { if (zoom >= min + step)  zoom -= step }}
+  />
+  <div class="zoom-number">
+    {zoom.toFixed(1)}x
+  </div>
+</div>
+
+<style>
+  .zoom-indicator {
+    color: #aaaaaa;
+
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    justify-items: center;
+    align-items: center;
+  }
+
+  .zoom-number {
+    display: flex;
+    font-size: 1.25em;
+    width: 2em;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .zoom-indicator:focus-within .zoom-number {
+    color: #eeeeee;
+  }
+
+  .zoom-range-container {
+    width: 2em;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .zoom-range-container:focus-within {
+    border-radius: 5px;
+    box-shadow: 0 0 0 1px  #FFFFFF;
+  }
+
+  input {
+    margin-bottom: 5%;
+    transform: rotate(270deg);
+    height: 10em;
+    position: relative;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+  input:focus::before {
+    content: '';
+    height: 100%;
+    width: 50%;
+    background: white;
+
+    outline: 1px solid #fff;
+    filter: drop-shadow(0 0 3px #fff);
+  }
+</style>
---
+++ src/lib/basic/IconButton.svelte
@@ -0,0 +1,30 @@
+<script type="ts">
+  import Icon from '@iconify/svelte'
+
+  import { createEventDispatcher } from 'svelte'
+  const dispatch = createEventDispatcher()
+
+  export let attrs: {
+    icon: string
+    height?: string
+    width?: string
+  }
+</script>
+
+<button on:click={() => {dispatch('click')}}>
+  <Icon {...attrs} />
+</button>
+
+<style>
+  button {
+    appearance: none;
+    background: transparent;
+    border: none;
+    color: inherit;
+    padding: 0;
+  }
+
+  button:hover {
+    filter: brightness(1.5);
+  }
+</style>
