8c2a9bbfe705a634ae79e5bceaf09f766e2e74ba
mkpoli <snowwolf1001@gmail.com>
2021-10-13 00:51:17 +0900

    Load package version as env

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="@sveltejs/kit" />
+interface ImportMeta {
+  env: {
+    PACKAGE_VERSION: string
+  }
+}
--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,26 @@
-import preprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess'
+import loadVersion from 'vite-plugin-package-version'
 import path from 'path'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-		vite: {
-			resolve: {
-				alias: {
-					'$assets': path.resolve('./src/assets')
-				}
-			}
-		}
-	}
-};
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    vite: {
+      resolve: {
+        alias: {
+          '$assets': path.resolve('./src/assets')
+        }
+      },
+      plugins: [
+        loadVersion()
+      ]
+    }
+  }
+}
 
-export default config;
+export default config
