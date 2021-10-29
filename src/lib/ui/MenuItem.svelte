caf3bca809b04d502fb343dda146a23231588ff8
mkpoli <snowwolf1001@gmail.com>
2021-10-29 19:14:36 +0900

    Combine MenuItemButton into MenuItem

--- src/lib/ui/MenuItem.svelte
+++ src/lib/ui/MenuItem.svelte
@@ -1,19 +1,52 @@
-<script lang="ts">
-  import Menu from "./Menu.svelte";
-  import MenuTrigger from "./MenuTrigger.svelte"
-  import MenuItemButton from "./MenuItemButton.svelte"
-
-  import Wrapper from "./Wrapper.svelte"
-
+<script lang="ts">  
+  import Icon from '@iconify/svelte'
+  
+  import Button from '$lib/ui/Button.svelte'
+  import Menu from '$lib/ui/Menu.svelte'
+  import MenuTrigger from '$lib/ui/MenuTrigger.svelte'  
+  import Wrapper from '$lib/ui/Wrapper.svelte'
+  
   export let icon: string
   export let text: string
   export let disabled: boolean = false
   export let checked: boolean = false
-
+  
   let container: HTMLDivElement = null
-
+  
   const hasSubMenu: boolean = !!$$slots.default
   let subMenu: HTMLDivElement
+  
+  
+  $: shortcutKey = text.match(/&([A-Z])/)?.[1]
+  
+  let altPressed: boolean
+  
+  import { createEventDispatcher } from "svelte"
+  const dispatch = createEventDispatcher()
+  
+  import { hideAll } from 'tippy.js'
+  function onclick() {
+    if (!hasSubMenu) {
+      hideAll()
+    }
+    dispatch('click')
+  }
+
+  function onkeydown(event: KeyboardEvent) {
+    if (event.key == 'Alt') {
+      altPressed = true
+    }
+
+    if (shortcutKey && event.altKey && shortcutKey.toLowerCase() === event.key ) {
+      onclick()
+    }
+  }
+  function onkeyup(event: KeyboardEvent) {
+    if (event.key == 'Alt') {
+      altPressed = false
+    }
+  }
+    
 </script>
 
 <Wrapper
@@ -22,19 +55,60 @@
   menu={subMenu}
   sub={true}
 >
-  <MenuItemButton 
-    {icon}
-    {text}
-    {disabled}
-    {hasSubMenu}
-    {checked}
-    bind:container
-    on:click
-  />
-  </Wrapper>
+  <div
+    class="menu-item"
+    role="menuitem"
+    bind:this={container}
+  >
+    <Button
+      {icon}
+      {disabled}
+      class="text"
+      on:click={onclick}
+    >
+      <div>{@html text.replace(/&([A-Z])/, `<span style="text-decoration: ${altPressed ? 'underline' : 'none'};">$1</span>`)}</div>
+      {#if hasSubMenu}
+        <Icon icon="ic:round-chevron-right" width="1.5em" class="chevron" />
+      {/if}
+      {#if checked}
+        <Icon icon="mdi:checkbox-marked-circle-outline" width="1.5em" class="chevron" />
+      {/if}
+    </Button>
+  </div>
+</Wrapper>
 
 {#if hasSubMenu}
   <Menu bind:menu={subMenu}>
     <slot/>
   </Menu>
 {/if}
+
+<svelte:window
+  on:keydown={onkeydown}
+  on:keyup={onkeyup}
+/>
+
+<style>
+  .menu-item :global(button) {
+    width: 100%;
+    justify-content: left;
+    border-radius: 0;
+  }
+
+  .menu-item :global(button:hover:not([disabled])) {
+    background-color: rgba(255, 255, 255, 0.25);
+  }
+
+  .menu-item :global(button[disabled]) {
+    filter:
+      blur(0.5px)
+      brightness(.85);
+    text-decoration: line-through;
+  }
+
+  .menu-item :global(.chevron) {
+    /* justify-self: flex-end; */
+    margin-left: auto;
+    margin-right: -0.5em;
+  }
+</style>
