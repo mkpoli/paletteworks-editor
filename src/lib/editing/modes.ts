b3d0f117cbba4e2f32a6b4a890e3bfe25b234d06
mkpoli <snowwolf1001@gmail.com>
2021-11-05 21:14:52 +0900

    D: Fix mode images not showing

--- src/lib/editing/modes.ts
+++ src/lib/editing/modes.ts
@@ -3,15 +3,20 @@ export type Mode = 'select' | 'tap' | 'slide' | 'mid' | 'flick' | 'critical' | '
 export const ALLOWED_SNAPPINGS = [4, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192] as const
 export type SnapTo = typeof ALLOWED_SNAPPINGS[number]
 
-import bpmImage from '$assets/BPM.png'
-import selectImage from '$assets/select.png'
+import bpm from '$assets/BPM.png'
+import select from '$assets/select.png'
+import tap from '$assets/notes/noteN.png'
+import slide from '$assets/notes/noteL.png'
+import mid from '$assets/notes/notes_long_among.png'
+import flick from '$assets/notes/noteL.png'
+import critical from '$assets/notes/noteC.png'
 type ImageSource = string
 export const MODE_TEXTURES: Record<Mode, ImageSource> = {
-  select: selectImage,
-  tap: 'noteN.png',
-  slide: 'noteL.png',
-  mid: 'notes_long_among.png',
-  flick: 'noteF.png',
-  critical: 'noteC.png',
-  bpm: bpmImage,
+  select,
+  tap,
+  slide,
+  mid,
+  flick,
+  critical,
+  bpm,
 }
