5ab6c2a3967c5cabddfbecb133e056947f76c4c2
mkpoli <snowwolf1001@gmail.com>
2022-01-15 19:54:52 +0900

    F: Show one toast at a time

--- src/lib/ui/toast.ts
+++ src/lib/ui/toast.ts
@@ -6,13 +6,16 @@ import type { SvelteComponent } from 'svelte'
 import type { Mutation } from '$lib/editing/mutations'
 import type { Writable } from 'svelte/store'
 
+let lastUndoToastId = 0
+
 export default {
   success: (message: string) => {
     toast.push(message, {
       theme: {
         '--toastBackground': '#48BB78',
         '--toastBarBackground': '#2F855A'
-      }
+      },
+      pausable: true,
     })
   },
   error: (message: string) => {
@@ -20,27 +23,35 @@ export default {
       theme: {
         '--toastBackground': '#F56565',
         '--toastBarBackground': '#C53030'
-      }
+      },
+      pausable: true,
     }
     )
   },
   undo: (mutation: Mutation<unknown>, history: Writable<Mutation<unknown>[]>, button: string, undo: () => void, undone: boolean) => {
-    toast.push({
-      component: {
-        src: UndoToast as unknown as typeof SvelteComponent,
-        props: {
-          text: mutation.toString(),
-          button,
-          undo,
-          mutation,
-          history,
-          undone
+    if (lastUndoToastId) {
+      toast.pop(lastUndoToastId) 
+    }
+
+    setTimeout(() => {
+      lastUndoToastId = toast.push({
+        component: {
+          src: UndoToast as unknown as typeof SvelteComponent,
+          props: {
+            text: mutation.toString(),
+            button,
+            undo,
+            mutation,
+            history,
+            undone
+          },
         },
-      },
-      theme: {
-        '--toastWidth': '20rem'
-      },
-      duration: 8000
-    })
+        theme: {
+          '--toastWidth': '20rem'
+        },
+        duration: 8000,
+        pausable: true,
+      })
+    }, lastUndoToastId ? 400 : 0)
   }
 }
