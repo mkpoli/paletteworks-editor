249c811c1afde0d4aaea80a4d70d819b3fcd063a
mkpoli <snowwolf1001@gmail.com>
2022-01-19 15:07:45 +0900

    F: Fix wrong URL in library api client

--- src/lib/api/library.ts
+++ src/lib/api/library.ts
@@ -23,7 +23,7 @@ export async function list(): Promise<Item[]> {
 }
 
 export async function create(item: Item): Promise<void> {
-  const res = await fetch('/library.json', {
+  const res = await fetch(URL, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
