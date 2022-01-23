f86a40503e6b887915dabef1304463ced007b0b1
mkpoli <snowwolf1001@gmail.com>
2022-01-23 10:57:01 +0900

    F: Make debug-display draggable

--- src/lib/ui/DebugInfo.svelte
+++ src/lib/ui/DebugInfo.svelte
@@ -1,9 +1,34 @@
 <script lang="ts">
   import { debugInfo } from '$lib/basic/debug'
   let hidden: boolean = false
+
+  let debugDisplay: HTMLDivElement
+  let offsetX: number = 0
+  let offsetY: number = 0
+
+  function movediv(event: PointerEvent) {
+    const { clientX, clientY } = event
+    debugDisplay.style.left = `${clientX - offsetX}px`
+    debugDisplay.style.top = `${clientY - offsetY}px`
+  }
 </script>
 
-<div class="debug-display" class:hidden on:dblclick={() => { hidden = true }}>
+<div
+  class="debug-display" class:hidden
+  on:dblclick={() => { hidden = true }}
+  on:pointerdown={(e) => {
+    if (e.button === 0) {
+      const { left, top } = debugDisplay.getBoundingClientRect()
+      offsetX = e.clientX - left
+      offsetY = e.clientY - top
+      window.addEventListener('pointermove', movediv)
+    }
+  }}
+  on:pointerup={() => {
+    window.removeEventListener('pointermove', movediv)
+  }}
+  bind:this={debugDisplay}
+>
   {#each [...$debugInfo.entries()] as [title, value] }
     <span class="title" title={title}>{title}</span>
     <span class="value">{value}</span>
@@ -21,7 +46,6 @@
     background: rgba(255, 255, 255, 0.3);
     backdrop-filter: blur(10px);
     box-shadow: 1px 1px 3px #000a;
-    border-bottom-left-radius: 1em;
     display: grid;
     grid-template-columns: auto 1fr;
     width: 30em;
