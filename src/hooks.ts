0913ba6130b7d759347917f2b3d0dd8ad5809258
mkpoli <snowwolf1001@gmail.com>
2022-02-22 05:16:53 +0900

    B: Fix build error

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,14 +1,22 @@
 import { detectLocale } from '$i18n/i18n-util'
-import type { GetSession } from '@sveltejs/kit'
+import type { GetSession, RequestEvent } from '@sveltejs/kit'
 import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors'
 
-export const getSession: GetSession = (request) => {
-	// detect the preferred language the user has configured in his browser
-	// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
-	const acceptLanguageDetector = initAcceptLanguageHeaderDetector(request)
-	const locale = detectLocale(acceptLanguageDetector)
+const getHeaders = (event: RequestEvent) => {
+  const headers: Record<string, string> = {}
+  event.request.headers.forEach((value, key) => headers[key] = value)
 
-	return {
-		locale,
-	}
+  return headers
+}
+
+export const getSession: GetSession = (event) => {
+  // detect the preferred language the user has configured in his browser
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Accept-Language
+  const headers = getHeaders(event)
+  const acceptLanguageDetector = initAcceptLanguageHeaderDetector({ headers })
+  const locale = detectLocale(acceptLanguageDetector)
+
+  return {
+    locale,
+  }
 }
