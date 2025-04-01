0df3a29d92cb51d2ed32769bd2e7765bf1c1c3b7
mkpoli <git@mkpo.li>
2025-04-01 07:58:50 +0000

    R: Adjust API file structure

--- api/library.ts
+++ api/library.ts
@@ -1,5 +1,53 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { list, create } from './api'
+
+import { createClient } from '@libsql/client/web'
+import type { Single, Slide } from '$lib/score/beatmap'
+
+type LocaleStrings = { [key: string]: string }
+
+export type Item = {
+  title: LocaleStrings
+  description: LocaleStrings
+  content: {
+    singles?: Single[]
+    slides?: Slide[]
+  }
+}
+
+const client = createClient({
+  url: process.env['TURSO_DATABASE_URL']!,
+  authToken: process.env['TURSO_AUTH_TOKEN']!,
+})
+
+export async function list(): Promise<Item[]> {
+  const res = await client.execute(
+    'SELECT title_ja, description_ja, content_json FROM items'
+  )
+
+  return res.rows.map((row) => ({
+    title: { ja: row.title_ja as string },
+    description: { ja: row.description_ja as string },
+    content: JSON.parse(row.content_json as string),
+  }))
+}
+
+export async function create(item: Item) {
+  await client.execute({
+    sql: `
+      INSERT INTO items (
+        id, collection, ts_iso, title_ja, description_ja, content_json
+      ) VALUES (?, ?, ?, ?, ?, ?)
+    `,
+    args: [
+      crypto.randomUUID(),
+      'items',
+      new Date().toISOString(),
+      item.title?.ja ?? null,
+      item.description?.ja ?? null,
+      JSON.stringify(item.content),
+    ],
+  })
+}
 
 export default async (request: VercelRequest, response: VercelResponse) => {
   try {
